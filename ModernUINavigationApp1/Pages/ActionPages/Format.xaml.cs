91ad250f3fac69a14eff3455b44b61559efa3306
anioa1998 <anioa1998@wp.pl>
2020-06-10 22:08:41 +0200

    ModernUI app

---
+++ ModernUINavigationApp1/Pages/ActionPages/Format.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ModernUINavigationApp1.Pages.ActionPages
+{
+    /// <summary>
+    /// Interaction logic for Format.xaml
+    /// </summary>
+    public partial class Format : UserControl
+    {
+        private Frame _navigationService;
+
+        public Format(Frame navigationService)
+        {
+            InitializeComponent();
+            _navigationService = navigationService;
+        }
+
+    }
+}
---
+++ ModernUINavigationApp1/Pages/MainMenu.xaml.cs
@@ -0,0 +1,55 @@
+﻿using FirstFloor.ModernUI.Windows.Controls;
+using ModernUINavigationApp1.Assets;
+using ModernUINavigationApp1.Pages.ActionPages;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ModernUINavigationApp1.Pages
+{
+    /// <summary>
+    /// Interaction logic for MainMenu.xaml
+    /// </summary>
+    public partial class MainMenu : UserControl
+    {
+        private Frame _navigationService;
+        public MainMenu(Frame navigationService)
+        {
+            InitializeComponent();
+            _navigationService = navigationService;
+        }
+
+
+
+        private void btnDiskInfo_Click(object sender, RoutedEventArgs e)
+        {
+            _navigationService.Navigate(new DiskInfo(_navigationService));
+
+        }
+
+        private void btnSmart_Click(object sender, RoutedEventArgs e)
+        {
+            _navigationService.Navigate(new Smart(_navigationService));
+        }
+
+        private void btnFormat_Click(object sender, RoutedEventArgs e)
+        {
+            _navigationService.Navigate(new Format(_navigationService));
+        }
+
+        private void btnCheck_Click(object sender, RoutedEventArgs e)
+        {
+            _navigationService.Navigate(new CheckDisk(_navigationService));
+        }
+
+        private void btnAbout_Click(object sender, RoutedEventArgs e)
+        {
+            _navigationService.Navigate(new About(_navigationService));
+        }
+
+        private void btnHelp_Click(object sender, RoutedEventArgs e)
+        {
+            _navigationService.Navigate(new Help(_navigationService));
+        }
+    }
+}
